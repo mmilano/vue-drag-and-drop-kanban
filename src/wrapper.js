b3cff47263c98633d446b583079b03d73f1ed693
mmilano <mmilano+git@gmail.com>
2020-12-11 12:40:18 -0800

    linting

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,28 +1,28 @@
 /* istanbul ignore file */
 // Import vue component
-import VueDragNDrop from './vue-drag-n-drop.vue';
+import VueDragNDrop from "./vue-drag-n-drop.vue";
 
 // Declare install function executed by Vue.use()
-export function install(Vue) {
-	if (install.installed) return;
-	install.installed = true;
-	Vue.component('vue-drag-n-drop', VueDragNDrop);
+export function install (Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component("vue-drag-n-drop", VueDragNDrop);
 }
 
 // Create module definition for Vue.use()
 const plugin = {
-	install,
+    install
 };
 
 // Auto-install when vue is found (eg. in browser via <script> tag)
 let GlobalVue = null;
-if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue;
+if (typeof window !== "undefined") {
+    GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+    GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-	GlobalVue.use(plugin);
+    GlobalVue.use(plugin);
 }
 
 // To allow use as module (npm/webpack/etc.) export component
