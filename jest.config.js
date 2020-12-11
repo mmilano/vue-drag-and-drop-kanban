2f821d28dcfa688cb766ee2ec32279b3d35ae8eb
mmilano <mmilano+git@gmail.com>
2020-12-11 12:34:57 -0800

    linted config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  "collectCoverage": true,
-  "collectCoverageFrom": ["src/**/*.{js,vue}", "!**/node_modules/**"]
-}
+    preset: "@vue/cli-plugin-unit-jest",
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.{js,vue}", "!**/node_modules/**"]
+};
