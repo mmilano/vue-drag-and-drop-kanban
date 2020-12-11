b298b3edcf0713ce765bdc532f06fd2a50b4c735
mmilano <mmilano+git@gmail.com>
2020-12-11 12:38:46 -0800

    linting, whitespace

--- src/App.vue
+++ src/App.vue
@@ -33,93 +33,93 @@ import DragDrop from "./vue-drag-n-drop.vue";
 import CustomCard from "./components/CustomCard.vue";
 
 export default {
-  name: "app",
-  components: {
-    DragDrop,
-    CustomCard
-  },
-  data() {
-    return{
-      stories: [
-        {
-          title: "Strategy 101",
-          description: "Create a draft of business plan",
-          time: "3 days",
-          done: false
-        },
-        {
-          title: "Strategy 102",
-          description: "Finalize the plan",
-          time: "4 days",
-          done: false
-        },
-        {
-          title: "Tech diagram",
-          description: "Draw the tech data",
-          time: "4 days",
-          done: false
-        },
-        {
-          title: "Task Holder",
-          description: "Data Science Team",
-          time: "5 days",
-          done: false
+    name: "app",
+    components: {
+        DragDrop,
+        CustomCard
+    },
+    data () {
+        return {
+            stories: [
+                {
+                    title: "Strategy 101",
+                    description: "Create a draft of business plan",
+                    time: "3 days",
+                    done: false
+                },
+                {
+                    title: "Strategy 102",
+                    description: "Finalize the plan",
+                    time: "4 days",
+                    done: false
+                },
+                {
+                    title: "Tech diagram",
+                    description: "Draw the tech data",
+                    time: "4 days",
+                    done: false
+                },
+                {
+                    title: "Task Holder",
+                    description: "Data Science Team",
+                    time: "5 days",
+                    done: false
+                }
+            ],
+
+            dropGroups: [
+                {
+                    name: "Business",
+                    children: []
+                },
+                {
+                    name: "Tech",
+                    children: []
+                },
+                {
+                    name: "Marketing",
+                    children: []
+                },
+                {
+                    name: "Accounting",
+                    children: []
+                }
+            ]
         }
-      ],
+    },
 
-      dropGroups: [
-        {
-          name: "Business",
-          children: []
+    methods: {
+        save (received) {
+            console.log("Received:", received);
         },
-        {
-          name: "Tech",
-          children: []
+
+        doneMarked (data) {
+            data.done = true;
+            alert(data.title);
         },
-        {
-          name: "Marketing",
-          children: []
+
+        originalBucketDropEvent (result) {
+            console.log("Original: ", result);
         },
-        {
-          name: "Accounting",
-          children: []
+
+        destinationBucketDropEvent (columnName, result) {
+            console.log("Destination: ", columnName, result);
+        },
+
+        cancel () {
+            console.log("Cancel hit");
         }
-      ]
     }
-  },
-
-  methods:{
-    save(received){
-      console.log("Received:", received)
-    },
-
-    doneMarked(data) {
-      data.done = true;
-      alert(data.title);
-    },
-
-    originalBucketDropEvent(result) {
-      console.log("Original: ", result);
-    },
-
-    destinationBucketDropEvent(columnName, result) {
-      console.log("Destination: ", columnName, result)
-    },
-
-    cancel() {
-      console.log("Cancel hit");
-    }
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
